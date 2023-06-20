60bf39b5963a6acab5e3b7b6b6aa1105fd59ddd5
Pedram Tavadze <petavazohi@mix.wvu.edu>
2023-06-20 00:06:29 -0400

    added powershell settings

---
+++ PowerShell/profile.ps1
@@ -0,0 +1,49 @@
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\ProgramData\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
+function source {
+    if (Test-Path $PROFILE.AllUsersAllHosts) { . $PROFILE.AllUsersAllHosts }
+    if (Test-Path $PROFILE.AllUsersCurrentHost) { . $PROFILE.AllUsersCurrentHost }
+    if (Test-Path $PROFILE.CurrentUserAllHosts) { . $PROFILE.CurrentUserAllHosts }
+    if (Test-Path $PROFILE.CurrentUserCurrentHost) { . $PROFILE.CurrentUserCurrentHost }
+}
+$targetDir = Split-Path $PROFILE.CurrentUserAllHosts
+oh-my-posh init pwsh --config "$targetDir\ohp-theme.json" | Invoke-Expression
+# function ConvertTo-RelativePath {
+#     param($Path)
+#     if ($Path.StartsWith($HOME)) {
+#         return $Path.Replace($HOME, "~")
+#     } else {
+#         return $Path
+#     }
+# }
+
+# function prompt {
+#     $user = $env:USERNAME
+#     $hst = $env:COMPUTERNAME
+#     $path = (Get-Location).Path
+#     $relativePath = ConvertTo-RelativePath $path
+#     $psVersion = $PSVersionTable.PSVersion
+    
+#     $CondaEnv = (Get-Content env:CONDA_PROMPT_MODIFIER -ErrorAction SilentlyContinue)
+
+#     if ($CondaEnv) {
+#         Write-Host -NoNewLine -ForegroundColor Magenta $CondaEnv
+#     }
+
+#     $GitBranch = (Get-Content env:__PS_GIT_BRANCH -ErrorAction SilentlyContinue)
+
+#     Get-Date -Format "ddd MMM dd hh:mm tt" | Write-Host -NoNewLine -ForegroundColor Red
+#     Write-Host "-[" -NoNewLine -ForegroundColor White
+#     Write-Host $user -NoNewLine -ForegroundColor Blue
+#     Write-Host "@" -NoNewLine -ForegroundColor White
+#     Write-Host $hst -NoNewLine -ForegroundColor Green
+#     Write-Host "]: " -NoNewLine -ForegroundColor White
+#     Write-Host -NoNewline -ForegroundColor Yellow $relativePath
+#     Write-Host 
+#     Write-Host "PS $($psVersion.Major).$($psVersion.Minor)>" -NoNewline -ForegroundColor White
+#     return " "
+# }
---
+++ setup.ps1
@@ -0,0 +1,38 @@
+# Check if 'oh-my-posh' is already installed
+winget list | Out-Null
+if (!(winget list --id "JanDeDobbeleer.OhMyPosh")) {
+    # If 'oh-my-posh' is not installed, install it
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+    Write-Host "oh-my-posh has been installed."
+} else {
+    # If 'oh-my-posh' is already installed, print a message
+    Write-Host "oh-my-posh is already installed."
+}
+
+# Define the source file - replace this with your actual file path
+$sourceFile = "PowerShell\profile.ps1"
+$targetDir = Split-Path $PROFILE.CurrentUserAllHosts
+# Check if the profile exists
+if (Test-Path $PROFILE.CurrentUserAllHosts) {
+    # Ask the user if they want to overwrite the existing profile
+    $overwrite = Read-Host "`$PROFILE.CurrentUserAllHosts already exists. Do you want to overwrite it? (Y/N)"
+    
+    # If the user chooses to overwrite the profile
+    if ($overwrite.ToUpper() -eq "Y") {
+        # Overwrite the profile with the content of the source file
+        Copy-Item $sourceFile $PROFILE.CurrentUserAllHosts -Force
+        Write-Host "`$PROFILE.CurrentUserAllHosts has been overwritten with the content of $sourceFile."
+    } else {
+        Write-Host "No changes have been made to `$PROFILE.CurrentUserAllHosts."
+    }
+} else {
+    # If the profile doesn't exist, create it with the content of the source file 
+    New-Item -Type File -Path $PROFILE.CurrentUserAllHosts -Force
+    Copy-Item $sourceFile $PROFILE.CurrentUserAllHosts -Force
+    Write-Host "`$PROFILE.CurrentUserAllHosts has been created with the content of $sourceFile
+    ."
+}
+Copy-Item PowerShell\ohp-theme.json $targetDir
+oh-my-posh init pwsh --config PowerShell\ohp-theme.json | Invoke-Expression
+. $PROFILE.CurrentUserAllHosts
+Write-Host "Do not forget to install fonts you can use 'oh-my-posh font install' in Admin PowerShell"
