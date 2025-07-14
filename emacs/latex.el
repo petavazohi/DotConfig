145e380b3e961f3a0a320becb4ca53f575e26905
petavazohi <petavazohi@mix.wvu.edu>
2025-07-14 16:29:23 -0400

    fixed latex.el

--- emacs/latex.el
+++ emacs/latex.el
@@ -12,3 +12,9 @@
 (add-hook 'TeX-mode-hook 'turn-on-reftex)
 
 
+(setq TeX-engine 'luatex)
+
+(eval-after-load "tex"
+  '(add-to-list 'TeX-command-list
+                '("LuaLaTeX" "%`lualatex%(mode) %t" TeX-run-TeX nil t)
+                t))
